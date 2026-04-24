b60a93b86bfdf49dc8fadb5677ade76b3609124a
LorenzoLatinnePXL <11701160@student.pxl.be>
2026-04-24 11:35:22 +0200

    Ex1-from-javascript-to-types

--- projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
@@ -1,4 +1,21 @@
 // Put your announceMachines function here! ✨
 // See ./original.js for its older JavaScript code.
+function announceMachines(announce, ...machines) {
+	let labelsCount = 0;
+
+	for (const m in machines) {
+		let label;
+
+		if (m.label) {
+			label = m.label;
+			labelsCount++;
+		} else {
+			label = `Male: ${m.make}; Model: ${m.model}`;
+		}
+		announce(label);
+	}
+
+	return labelsCount;
+}
 
 module.exports.announceMachines = announceMachines;
